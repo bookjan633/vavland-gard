1e72bc15a8a786e3ad3624761789a3820df15999
Jan Book <janbook@MacBook-Pro-som-tillhor-Jan.local>
2026-08-05 15:29:04 +0200

    Disable page cache

--- app/ovre-bostadshus/page.tsx
+++ app/ovre-bostadshus/page.tsx
@@ -1,25 +1,45 @@
+export const dynamic = "force-dynamic";
+
+export const fetchCache = "force-no-store";
+
 import BuildingPage from "@/components/buildings/BuildingPage";
+
 import { ovreBostadshus } from "@/data/buildings/ovre-bostadshus";
 
 import { client } from "@/sanity/lib/client";
+
 import { pageQuery } from "@/sanity/lib/queries";
 
 export default async function Page() {
-  const cmsPage = await client.fetch(pageQuery, {
-    slug: "ovre-bostadshus",
-  });
 
-  console.log(cmsPage);
+  const cmsPage = await client.fetch(
+
+  pageQuery,
+
+  {
+
+    slug: "ovre-bostadshus",
+
+  },
+
+  {
+
+    cache: "no-store",
+
+  }
+
+);
 
   return (
 
-  <BuildingPage
+    <BuildingPage
 
-    building={ovreBostadshus}
+      building={ovreBostadshus}
 
-    cmsPage={cmsPage}
+      cmsPage={cmsPage}
 
-  />
+    />
 
-);
+  );
+
 }
